8daf4a1445cf377aff8a348ec710d82c903f5b80
Ivan Tabarino <tabarino@outlook.com>
2021-01-16 20:13:11 +0000

    Adding Data to an ASP.NET Core with Angular

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -5,8 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-  debugger;
-
   title = 'app';
 }
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.Angular.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Maker { get; set; }
+
+        [JsonPropertyName("Img")]
+        public string Image { get; set; }
+
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int[] Rating { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
